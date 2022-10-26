f6b01c63e34e834751b66f8f574e234af49e62df
bryan kretz <kretzbryan@gmail.com>
2022-10-26 00:05:28 -0700

    transitions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router';
-import Splash from './components/Splash';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import About from './components/About';
-import Header from './components/Header';
+
 import Main from './components/Main';
-import ExternalLinkContainer from './components/ExternalLinkContainer';
 
 function App() {
 	return (
@@ -16,9 +8,6 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<Main />} />
-					<Route path='/skills' element={<Skills />} />
-					<Route path='/projects' element={<Projects />} />
-					<Route path='/about' element={<About />} />
 				</Routes>
 			</BrowserRouter>
 		</>
